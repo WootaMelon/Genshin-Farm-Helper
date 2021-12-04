8b30173f0dfd5ace8e74afa8157ed805da1c0950
Mohamad Mehdi <84543698+WootaMelon@users.noreply.github.com>
2021-12-05 01:19:00 +0200

    Added WeaponAscension Page
    Completed WeaponAscension Page and its link to the menu

--- src/app/weaponascension/weaponascension.page.ts
+++ src/app/weaponascension/weaponascension.page.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weaponascension.page.scss'],
 })
 export class WeaponascensionPage implements OnInit {
+  days: string
+  constructor() {
+    let currentDate = new Date();
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    this.days = weekdays[currentDate.getDay()];
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
