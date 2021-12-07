5eeb9494e005e49b6032dbd6510256e57b872177
Mohamad Mehdi <84543698+WootaMelon@users.noreply.github.com>
2021-12-08 01:44:32 +0200

    Attempting to display weekly timer

--- src/app/farmselectorpage/farmselectorpage.page.ts
+++ src/app/farmselectorpage/farmselectorpage.page.ts
@@ -13,9 +13,12 @@ export class FarmselectorpagePage implements OnInit {
 
   resincount: string
 
+  weeklyReset: string
   constructor(public modalCtrl: ModalController, private router: Router, private storage: Storage) {
 
   }
+
+
   // public CharacterSelector(){
   //   this.router.navigate(['characterscroll']);
   // }
@@ -33,8 +36,38 @@ export class FarmselectorpagePage implements OnInit {
     setTimeout(() => {
       this.storage.get('resin').then((val) => {
         this.resincount = val;
+
       });
     }, 100);
+
+  }
+
+  WeeklyCountDown() {
+    let d = new Date();
+    d.setHours(5);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    var day = d.getDay(),
+      diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+    let countDownDate = d.setDate(diff + 7);
+    // Update the count down every 1 second
+    let x = setInterval(function () {
+
+      // Get todays date and time
+      let now = new Date().getTime();
+      // Find the distance between now and the count down date
+      let distance = countDownDate - now;
+      // Time calculations for days, hours, minutes and seconds
+      let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+      // Output the result in an element with id="demo"
+      this.weeklyReset = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+    }, 1000);
   }
 
 }
